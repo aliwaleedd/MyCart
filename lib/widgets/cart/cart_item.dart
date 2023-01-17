d54c2329458d68cd54317166128052040837ef7a
aliwaleedd <103014509+aliwaleedd@users.noreply.github.com>
2023-01-17 18:49:57 +0200

    Update cart_item.dart

--- lib/widgets/cart/cart_item.dart
+++ lib/widgets/cart/cart_item.dart
@@ -12,9 +12,14 @@ class CartItem extends StatefulWidget {
   double price;
   int iniValue;
   Function reCalculate;
-
-  
-
+  CartItem(CartItemClass cItem, Function reCalculate) {
+    this.mItem = cItem.mItem;
+    this.title = cItem.mItem.getName();
+    this.picture = cItem.mItem.getImagePath();
+    this.price = cItem.totalPrice;
+    this.iniValue = cItem.qty;
+    this.reCalculate = reCalculate;
+  }
   @override
   CartItemState createState() {
     return CartItemState();
