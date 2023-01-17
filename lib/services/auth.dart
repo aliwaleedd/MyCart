713c0eade9cfb4056c8a8d9bffebc266ab973c06
aliwaleedd <103014509+aliwaleedd@users.noreply.github.com>
2023-01-17 18:56:15 +0200

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -50,3 +50,50 @@ class Auth {
     return authresult;
   }
 }
+class Auth {
+  final _auth = FirebaseAuth.instance;
+  final Future<FirebaseApp> _future = Firebase.initializeApp();
+  final databaseRef = FirebaseDatabase.instance.reference().child('users');
+
+  Future signUp(name, phone, email, password) async {
+    final authresult = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    databaseRef.child(_auth.currentUser.uid).set({
+      'name': name.toString(),
+      'phone': phone.toString(),
+      'email': email.toString(),
+      'type': 'user',
+    });
+    return authresult;
+  }
+
+  Future signIn(String email, String password) async {
+    final authresult = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    var name = (await FirebaseDatabase.instance
+            .reference()
+            .child('users')
+            .child(_auth.currentUser.uid)
+            .child("name")
+            .once())
+        .value;
+    var phone = (await FirebaseDatabase.instance
+            .reference()
+            .child('users')
+            .child(_auth.currentUser.uid)
+            .child("phone")
+            .once())
+        .value;
+    var type = (await FirebaseDatabase.instance
+            .reference()
+            .child('users')
+            .child(_auth.currentUser.uid)
+            .child("type")
+            .once())
+        .value;
+    await DataManager.mPrefManager.setLoggedInData(
+        _auth.currentUser.uid, name, phone, email, password, type);
+    return authresult;
+  }
+}
+
